c67a19aac0234196ec101dca10a852ff98dff65d
Rob Nelson <nexis@7chan.org>
2013-07-13 23:29:31 -0700

    OH BOY HERE WE GO, BIGROCK TIME BABY

--- checkForErrors.bat
+++ checkForErrors.bat
@@ -2,6 +2,7 @@
 python python/fixPopfiles.py src/30wave/mvm_mannworks_30wave.pop -o processed/30wave/mvm_mannworks_30wave.pop -s mvm_mannworks.spawns.txt
 python python/fixPopfiles.py src/30wave/mvm_coaltown_30wave.pop -o processed/30wave/mvm_coaltown_30wave.pop -s mvm_coaltown.spawns.txt
 python python/fixPopfiles.py src/30wave/mvm_decoy_30wave.pop -o processed/30wave/mvm_decoy_30wave.pop -s mvm_decoy.spawns.txt
+python python/fixPopfiles.py src/30wave/mvm_bigrock_30wave.pop -o processed/30wave/mvm_bigrock_30wave.pop -s mvm_bigrock.spawns.txt
 
 :: 7wave
 python python/fixPopfiles.py src/7wave/mvm_coaltown_7waves.pop -i includes/robot_standard_7waves.pop includes/robot_giant_7waves.pop -s mvm_coaltown.spawns.txt -o processed/7wave/mvm_coaltown_7waves.pop 
